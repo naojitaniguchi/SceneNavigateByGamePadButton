5ba8de10c0e48d0b2fe6bda11cfbde958a2efbd0
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-11 09:43:17 +0900

    First commit.

---
+++ Assets/SceneNavigate.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigate : MonoBehaviour
+{
+    bool descriptionShowing = false;
+    public GameObject descriptionImage;
+    public string startScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 9"))
+        {
+            Debug.Log("button9");
+            controllDescription();
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            controllDescription();
+        }
+
+        if (descriptionShowing)
+        {
+            // A button
+            if (Input.GetKeyDown("joystick button 16"))
+            {
+                Debug.Log("button16");
+                SceneManager.LoadScene(startScene);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("A");
+                SceneManager.LoadScene(startScene);
+            }
+        }
+    }
+
+    void controllDescription()
+    {
+        if (descriptionShowing)
+        {
+            descriptionShowing = false;
+            descriptionImage.SetActive(false);
+        }
+        else
+        {
+            descriptionShowing = true;
+            descriptionImage.SetActive(true);
+        }
+    }
+
+    
+}
